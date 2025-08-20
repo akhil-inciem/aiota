9ec86e55d98b39d9670f0b82fc0e5dbaf02f7aca
Akhil Kumar M <itzmeakhil26@gmail.com>
2025-08-20 12:08:04 +0530

    custom button & font

--- lib/views/custom_widgets/custom_textfield.dart
+++ lib/views/custom_widgets/custom_textfield.dart
@@ -1,8 +1,19 @@
+import 'package:aiota/constants/colors.dart';
 import 'package:flutter/material.dart';
 
 class CustomTextfield extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return TextField(
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: AppColors.primaryBackground,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8.0),
+          borderSide: BorderSide.none,
+        ),
+        hintText: 'Enter text',
+      ),
+    );
   }
 }
